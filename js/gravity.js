4454f8db5b566ec2bc1cd5193f1f3386bdbd623c
nielskornerup <nielsskornerup@gmail.com>
2015-05-08 10:16:50 -0500

    version 2.0

--- js/gravity.js
+++ js/gravity.js
@@ -1,20 +1,22 @@
 var canvas = document.getElementById("grav-canvas");
 var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth*2/3-10;
+canvas.height = window.innerHeight-10;
 var width = canvas.width;
 var height = canvas.height;
 var Speed = 2;
 var MaxSize= 30;
 var numGrav = 5;
 var allParticles=[];
-var gravConstant = 50;
+var gravConstant = 2;
 var PI = 3.141592;
-var maxSpeed = 10;
+var maxSpeed = 30;
 var maxDist =900;
-var collisions=false;
+var collisions=true;
 
 function initializeGrav() {
 	document.getElementById('numparticles').value=numGrav;
-	document.getElementById('gravstr').value=gravConstant;
+	document.getElementById('gravstr').value=gravConstant*50;
 	for (var i = 0; i < numGrav; i++) {
 		var grav = new Object();
 		grav.radius = Math.random() * MaxSize;
@@ -25,8 +27,8 @@ function initializeGrav() {
 		grav.newVelocity = new Object();
 		grav.location.x = (Math.random() * (width-5))+5;
 		grav.location.y = (Math.random() * (height-grav.radius))+grav.radius;
-		grav.velocity.x = Math.random() * maxSpeed;
-		grav.velocity.y = Math.random() * maxSpeed;
+		grav.velocity.x = Math.random() * 5;
+		grav.velocity.y = Math.random() * 5;
 		allParticles.push(grav);
 		grav.newVelocity.y=grav.velocity.y;
 		grav.newVelocity.x=grav.velocity.x;
@@ -79,31 +81,29 @@ function bounce(allParticles){
 };
 
 function moveGrav(){
-	if(collisions){
-		bounce(allParticles);
-	}
 	for (var x = 0; x < allParticles.length; x++) {
 		for (var y = x; y < allParticles.length; y++) {
 			var difX = (allParticles[x].location.x-allParticles[y].location.x);
 			var difY = (allParticles[x].location.y-allParticles[y].location.y);
 			var dist =Math.sqrt((Math.pow(difX,2)+Math.pow(difY,2)));
-			if(dist>5&&dist<maxDist){
-				allParticles[x].velocity.x-=(difX*allParticles[y].radius*gravConstant)/Math.pow(dist,3);
-				allParticles[x].velocity.y-=(difY*allParticles[y].radius*gravConstant)/Math.pow(dist,3);
-				allParticles[y].velocity.x+=(difX*allParticles[x].radius*gravConstant)/Math.pow(dist,3);
-				allParticles[y].velocity.y+=(difY*allParticles[x].radius*gravConstant)/Math.pow(dist,3);
+			if(dist>allParticles[x].radius+allParticles[y].radius){
+				allParticles[x].velocity.x-=(difX*allParticles[y].mass*gravConstant)/Math.pow(dist,3);
+				allParticles[x].velocity.y-=(difY*allParticles[y].mass*gravConstant)/Math.pow(dist,3);
+				allParticles[y].velocity.x+=(difX*allParticles[x].mass*gravConstant)/Math.pow(dist,3);
+				allParticles[y].velocity.y+=(difY*allParticles[x].mass*gravConstant)/Math.pow(dist,3);
 			}
 		}
 	}
+	if(collisions){
+		bounce(allParticles);
+	}
 	for (var x = 0; x < allParticles.length; x++) {
-		if(allParticles[x].velocity.x>maxSpeed){
-			allParticles[x].velocity.x=maxSpeed;
+		if (Math.abs(allParticles[x].velocity.x) > maxSpeed){
+			allParticles[x].velocity.x = Math.abs(allParticles[x].velocity.x)*maxSpeed/(allParticles[x].velocity.x);
 		}
-		if(allParticles[x].velocity.y>maxSpeed){
-			allParticles[x].velocity.y=maxSpeed;
+		if (Math.abs(allParticles[x].velocity.y) > maxSpeed){
+			allParticles[x].velocity.y = Math.abs(allParticles[x].velocity.y)*maxSpeed/(allParticles[x].velocity.y);
 		}
-		allParticles[x].location.x+=allParticles[x].velocity.x;
-		allParticles[x].location.y+=allParticles[x].velocity.y;
 		if (allParticles[x].location.x >= canvas.width-(allParticles[x].radius/2)) {
 			allParticles[x].velocity.x=-Math.abs(allParticles[x].velocity.x);
 		}
@@ -116,6 +116,8 @@ function moveGrav(){
 		if(allParticles[x].location.y <= (allParticles[x].radius)){
 			allParticles[x].velocity.y = Math.abs(allParticles[x].velocity.y);
 		}
+		allParticles[x].location.x+=allParticles[x].velocity.x;
+		allParticles[x].location.y+=allParticles[x].velocity.y;
 	}
 }
 
@@ -138,11 +140,19 @@ function drawGrav() {
 initializeGrav();
 
 $("#controls-submit").click(function() {
-	numGrav = $("#numparticles").val();
-	gravConstant = $("#gravstr").val();
-	collisions = $("#collisions").is(":checked");
-	allParticles = [];
-	initializeGrav();
+	if($("#numparticles").val()>15||$("#numparticles").val()<0){
+		alert("Please use between 0 and 15 particles");
+	}
+	else if($("#gravstr").val()>250||$("#gravstr").val()<-250){
+		alert("Please make gravity's strength between -250 and 250");
+	}
+	else{
+		numGrav = $("#numparticles").val();
+		gravConstant = $("#gravstr").val()/50;
+		collisions = $("#collisions").is(":checked");
+		allParticles = [];
+		initializeGrav();
+	}
 });
 
 function main() {
